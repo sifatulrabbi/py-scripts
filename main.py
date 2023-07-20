3d925c9234617dd6ff995c246fffdcdd40a7edb0
Md Sifatul Islam Rabbi <mdsifatulislam.rabbi@gmail.com>
2023-07-20 14:33:43 +0600

    testing api

--- main.py
+++ main.py
@@ -1,18 +1,87 @@
 import stripe
 import os
+import requests
+import json
+from time import sleep
 from dotenv import load_dotenv
 from pprint import pprint
-
+from tool_category_list import categories
 
 load_dotenv()
 stripe.api_key = os.getenv("STRIPE_LIVE_API_KEY")
 
+
+def get_backend_url(path: str):
+    return "http://localhost:3001/api" + path
+
+
+def get_data(type: str):
+    data = {
+        "type": type,
+        "tones": "",
+        "target_lang": "EN-US",
+        "description": "give me a content strategy for a spring water brand",
+        "descriptionHTML": "<p>give me a content strategy for a spring water brand</p>",
+        "source_lang": "EN-US",
+        "journalistName": "",
+        "domains": "",
+        "company": "",
+        "name": "",
+        "role": "",
+    }
+    return json.dumps(data)
+
+
+infected_arr = {}
+
 try:
-    customers = stripe.Customer.list(email="sifatuli.r@gmail.com")
-    for cus in customers.list():
-        pprint(cus)
-except stripe.error.StripeError as err:
-    print(err)
+    for category in categories:
+        print("----------------------------------")
+        print(f"using -> {category['path']}")
+        sleep(2)
+        for tool in category["tools"]:
+            print(f"testing -> {tool}")
+            try:
+                path = category["path"]
+                response = requests.post(
+                    url=get_backend_url(path),
+                    headers={
+                        "Authorization": "Bearer " + os.getenv("AUTH0_TOKEN"),
+                        "Content-Type": "application/json",
+                    },
+                    data=get_data(tool),
+                )
+                print(f"    {response.status_code} {response.reason}")
+                try:
+                    result = response.json()
+                    contents = result["results"]
+                    if len(contents) > 1:
+                        uniques = []
+                        for content in contents:
+                            text = content["text"]
+                            if text in uniques:
+                                infected_arr.setdefault(path, []).append(tool)
+                                print(f"    {tool} tool has duplicate contents")
+                            else:
+                                uniques.append(text)
+                except requests.exceptions.JSONDecodeError as err:
+                    print(f"    error: unable to parse json. {err.strerror}")
+            except requests.exceptions.HTTPError as err:
+                print(f"    error: {tool} tool isn't working")
+except:
+    print("Loop closed unexpectedly")
+
+
+with open("./tmp/tools-similar-contents.json", "w") as f:
+    json.dump(infected_arr, f)
+    f.close()
+
+# try:
+# customers = stripe.Customer.list(email="sifatuli.r@gmail.com")
+# for cus in customers.list():
+#     pprint(cus)
+# except stripe.error.StripeError as err:
+#     print(err)
 
 
 # users: list[User] = []
---
+++ test_dict.py
@@ -0,0 +1,5 @@
+my_dict = {}
+my_dict.setdefault("name", {})["first"] = "Sifatul"
+my_dict.setdefault("hobbies", []).append("Coding")
+my_dict.setdefault("hobbies", []).append("Gym")
+print(my_dict)
